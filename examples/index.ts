1231f6ed27b7c9838aca61b3c6c4955d14f8d3db
Mijura <miodragvilotijevic@gmail.com>
2025-11-03 12:17:25 +0100

    revert removed index.ts

---
+++ examples/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+
+const message = "I'm planning a weekend trip. What's the weather in Belgrade?"
+
+import { Gpt5Nano } from "@jigjoy-io/mosaic"
+import { WeatherDB, WeatherTool } from "./tools/weather"
+
+const gpt5Nano = new Gpt5Nano()
+
+//tools
+let weatherTool: WeatherTool = new WeatherTool(new WeatherDB())
+
+gpt5Nano
+    .context([{role: 'user', content: message}])
+    .tools([weatherTool])
+
+const exec = await gpt5Nano.send()
+
+console.log("ToolCalls →", exec.toolCalls)
+console.log("Answer →", exec.text)
