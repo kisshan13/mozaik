a2cc27609c35d3a1ddc46e04b5b97b09978cffad
Mijura <miodragvilotijevic@gmail.com>
2026-03-29 14:14:18 +0200

    interact method

--- src/domain/hypervisor/participant.ts
+++ src/domain/hypervisor/participant.ts
@@ -1,13 +1,20 @@
+import { Environment } from "./environment"
 import { Interaction } from "./interaction"
 import { Tool } from "./tool"
 
 export abstract class Participant {
 	readonly id: string
 	tools: Tool[]
+	environment: Environment
 
-	constructor(id: string, tools: Tool[]) {
+	constructor(id: string, tools: Tool[], environment: Environment) {
 		this.id = id
 		this.tools = tools
+		this.environment = environment
+	}
+
+	interact(interaction: Interaction<unknown>, tool: Tool): Promise<unknown> {
+		return this.environment.absorb(interaction, tool)
 	}
 
 	abstract observe(interaction: Interaction<unknown>): void
