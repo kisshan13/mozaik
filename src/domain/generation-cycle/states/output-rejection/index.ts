dcdee5f75908cc4c2e5a7873208589c5ef5004e0
Mijura <miodragvilotijevic@gmail.com>
2026-04-02 15:08:50 +0200

    output rejection state

--- src/domain/generation-cycle/states/output-rejection/index.ts
+++ src/domain/generation-cycle/states/output-rejection/index.ts
@@ -1,7 +1,8 @@
-import { GoTo, GenerationContext, State, StateId, Transition } from "../../generation-context"
+import { GoTo, GenerationContext, State, StateId, Transition, GenerationStatus } from "../../generation-context"
 
 export class OutputRejection implements State {
 	async run(generationContext: GenerationContext): Promise<Transition> {
-		return new GoTo(StateId.OUTPUT_EXECUTION)
+		generationContext.status = GenerationStatus.FAILED
+		return new GoTo(StateId.CYCLE_END)
 	}
 }
