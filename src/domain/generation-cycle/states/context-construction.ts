b8729ed57b49dab08bbdfa34b576bf1cf5f33914
Mijura <miodragvilotijevic@gmail.com>
2026-04-03 11:26:41 +0200

    states folder structure simplified

---
+++ src/domain/generation-cycle/states/context-construction.ts
@@ -0,0 +1,7 @@
+import { GoTo, SessionContext, State, StateId, Transition } from "../state"
+
+export class ContextConstruction implements State {
+	async run(sessionContext: SessionContext): Promise<Transition> {
+		return new GoTo(StateId.INFERENCE)
+	}
+}
---
+++ src/domain/generation-cycle/states/generation-cycle-end.ts
@@ -0,0 +1,7 @@
+import { GoTo, SessionContext, State, StateId, Transition } from "../state"
+
+export class GenerationCycleEnd implements State {
+	async run(sessionContext: SessionContext): Promise<Transition> {
+		return new GoTo(StateId.CONTEXT_CONSTRUCTION)
+	}
+}
