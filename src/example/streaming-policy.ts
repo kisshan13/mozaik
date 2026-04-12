afcf414989e500836044040f7557e48a93468b24
Mijura <miodragvilotijevic@gmail.com>
2026-04-12 08:18:03 +0200

    removed stale policy

--- src/example/streaming-policy.ts
+++
@@ -1,7 +0,0 @@
-import { BaseCondition } from "src/domain/context-engine/condition/base-condition"
-
-export class StreamingPolicy extends BaseCondition<any> {
-	isSatisfiedBy(candidate: any): boolean {
-		return candidate.delta
-	}
-}
